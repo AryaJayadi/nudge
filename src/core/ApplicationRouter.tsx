b9391835c371cecb21b18f4a438cdf7d61a498dc
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-02-25 20:47:12 +0700

    feat: working routing

--- src/core/ApplicationRouter.tsx
+++ src/core/ApplicationRouter.tsx
@@ -1,7 +1,70 @@
-const ApplicationRouter = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import {JSX} from "react";
+import {createBrowserRouter, Navigate, Outlet, useLocation} from "react-router";
+import {AuthLayout} from "@/presentation/template/AuthLayout.tsx";
+import {RootLayout} from "@/presentation/template/RootLayout.tsx";
+import {LoginPage} from "@/presentation/auth/LoginPage.tsx";
+import {RouterProvider} from "react-router/dom";
+
+function isAuthenticated(): boolean {
+    return true;
 }
+
+interface ProtectedRouteProps {
+    isAuthenticated: () => boolean;
+    redirectPath: string;
+}
+
+const ProtectedRoute: (p: ProtectedRouteProps) => (JSX.Element) = (p: ProtectedRouteProps) => {
+    const location = useLocation()
+
+    if (!p.isAuthenticated()) {
+        return <Navigate to={p.redirectPath} state={{from: location}}/>
+    }
+
+    return <Outlet/>
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout/>,
+        children: [
+            {
+                path: "",
+                element: <ProtectedRoute isAuthenticated={isAuthenticated} redirectPath={"/auth/login"}/>,
+                children: [
+                    {
+                        // path: "",
+                        // element: <DashboardLayout/>,
+                        // children: [
+                        //     {
+                        //         path: "company",
+                        //         element: <CompanyPage/>
+                        //     },
+                        //     {
+                        //         path: "student",
+                        //         element: <StudentPage/>
+                        //     },
+                        // ]
+                    },
+                ]
+            },
+            {
+                path: "auth",
+                element: <AuthLayout/>,
+                children: [
+                    {
+                        path: "login",
+                        element: <LoginPage/>
+                    }
+                ]
+            }
+        ]
+    }
+]);
+
+export const ApplicationRouter = () => {
+    return (
+        <RouterProvider router={router}/>
+    );
+};
