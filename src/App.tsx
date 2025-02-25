5fb8b7e19662f1e51c7c8052738e23aa02e2071a
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-02-25 20:51:08 +0700

    feat: theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import {ApplicationRouter} from "@/core/ApplicationRouter.tsx";
 import {Toaster} from "@/components/ui/toaster.tsx";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
 
 
 function App() {
-  return (
-    <div>
-      <ApplicationRouter />
-      <Toaster />
-    </div>
-  );
+    return (
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <ApplicationRouter/>
+            <Toaster/>
+        </ThemeProvider>
+    );
 }
 
 export default App;
