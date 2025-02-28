af42cbb20fc70d0b3f4b5f58ab81a47247d0b5ae
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-02-28 19:16:07 +0700

    fix: remove scroll bars

--- src/presentation/template/RootLayout.tsx
+++ src/presentation/template/RootLayout.tsx
@@ -2,7 +2,7 @@ import {Outlet} from "react-router";
 
 export const RootLayout = () => {
     return (
-        <div className="w-screen h-screen">
+        <div className="w-screen h-screen overflow-hidden">
             <Outlet />
         </div>
     )
