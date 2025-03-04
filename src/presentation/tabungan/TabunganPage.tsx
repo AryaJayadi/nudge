9d0d2ca1e5db6ac746588b94cc1ce2fe6d2d42c5
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-04 23:34:54 +0700

    feat: implement loading

--- src/presentation/tabungan/TabunganPage.tsx
+++ src/presentation/tabungan/TabunganPage.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/product-card.tsx";
 import {PiggyBank} from "lucide-react";
 import useViewModel from "./TabunganPageViewModel.ts"
+import SkeletonCard from "@/components/skeleton-card.tsx";
 
 export const TabunganPage = () => {
     const {
@@ -8,24 +9,31 @@ export const TabunganPage = () => {
         records
     } = useViewModel();
 
+    const length = 4;
+
     return (
         <>
-            {records.map((o, index) => (
-                <ProductCard
-                    key={index}
-                    title={o.record_title}
-                    description={o.record_description}
-                    icon={<PiggyBank className="h-6 w-6" />}
-                    isPopular={true}
-                    primaryInfo={{ label: "Bunga", value: "2,5% p.a." }}
-                    secondaryInfo={{ label: "Setoran Minimal", value: "Rp 50.000" }}
-                    features={["test1", "test2", "test3"]}
-                    benefits={["test1", "test2", "test3"]}
-                    risks={["test1", "test2", "test3"]}
-                    ctaText="Buka Sekarang"
-                    onCtaClick={() => console.log(`Buka ${o.record_title}`)}
-                />
-            ))}
+            {
+                loading ?
+                    Array.from({length: length}).map((_, i) => (
+                        <SkeletonCard/>
+                    )) :
+                    records.map((o, index) => (
+                        <ProductCard
+                            key={index}
+                            title={o.record_title}
+                            description={o.record_description}
+                            icon={<PiggyBank className="h-6 w-6"/>}
+                            isPopular={true}
+                            primaryInfo={{label: "Bunga", value: "2,5% p.a."}}
+                            secondaryInfo={{label: "Setoran Minimal", value: "Rp 50.000"}}
+                            features={["test1", "test2", "test3"]}
+                            benefits={["test1", "test2", "test3"]}
+                            risks={["test1", "test2", "test3"]}
+                            ctaText="Buka Sekarang"
+                            onCtaClick={() => console.log(`Buka ${o.record_title}`)}
+                        />
+                    ))}
         </>
     )
 }
