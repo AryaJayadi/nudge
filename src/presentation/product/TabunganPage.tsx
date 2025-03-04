8e1146ec45d52250b56784c4808adf1a42e08fc3
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-05 03:11:20 +0700

    feat: implemented RecordCard

--- src/presentation/product/TabunganPage.tsx
+++ src/presentation/product/TabunganPage.tsx
@@ -3,6 +3,7 @@ import {PiggyBank} from "lucide-react";
 import useViewModel from "./ProductPageViewModel.ts"
 import SkeletonCard from "@/components/skeleton-card.tsx";
 import {RecordCategory} from "@/domain/interface/RecordCategory.ts";
+import RecordCard from "@/components/record-card.tsx";
 
 export const TabunganPage = () => {
     const {
@@ -20,20 +21,29 @@ export const TabunganPage = () => {
                         <SkeletonCard/>
                     )) :
                     records.map((o, index) => (
-                        <ProductCard
-                            key={index}
-                            title={o.record_title}
-                            description={o.record_description}
-                            icon={<PiggyBank className="h-6 w-6"/>}
-                            isPopular={true}
-                            primaryInfo={{label: "Bunga", value: "2,5% p.a."}}
-                            secondaryInfo={{label: "Setoran Minimal", value: "Rp 50.000"}}
-                            features={["test1", "test2", "test3"]}
-                            benefits={["test1", "test2", "test3"]}
-                            risks={["test1", "test2", "test3"]}
-                            ctaText="Buka Sekarang"
-                            onCtaClick={() => console.log(`Buka ${o.record_title}`)}
-                        />
+                        <>
+                            <RecordCard
+                                title={o.record_title}
+                                description={o.record_description}
+                                profit={12.5}
+                                price={5000000}
+                                risk={30}
+                            />
+                            <ProductCard
+                                key={index}
+                                title={o.record_title}
+                                description={o.record_description}
+                                icon={<PiggyBank className="h-6 w-6"/>}
+                                isPopular={true}
+                                primaryInfo={{label: "Bunga", value: "2,5% p.a."}}
+                                secondaryInfo={{label: "Setoran Minimal", value: "Rp 50.000"}}
+                                features={["test1", "test2", "test3"]}
+                                benefits={["test1", "test2", "test3"]}
+                                risks={["test1", "test2", "test3"]}
+                                ctaText="Buka Sekarang"
+                                onCtaClick={() => console.log(`Buka ${o.record_title}`)}
+                            />
+                        </>
                     ))}
         </>
     )
