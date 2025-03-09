1b87ce9dbd72f8193145681831365888cfa87451
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-09 21:06:28 +0700

    feat: implemented questionnaireContext

--- src/presentation/questionnaire/QuestionnairePage.tsx
+++ src/presentation/questionnaire/QuestionnairePage.tsx
@@ -1,22 +1,25 @@
-import { useState } from "react"
+import {useState} from "react"
 import "@/styles/globals.css"
 import ConsentModal from "@/components/consent-modal"
 import Questionnaire from "@/components/questionnaire"
-import useViewModel from "./QuestionnairePageViewModel.tsx"
+import {QuestionnaireProvider, useQuestionnaire} from "@/presentation/context/QuestionnaireContext.tsx";
 
 export const QuestionnairePage = () => {
     const {
         loading,
         questions
-    } = useViewModel();
+    } = useQuestionnaire();
     const [hasConsented, setHasConsented] = useState(false)
 
     return (
-        <main className="min-h-screen flex items-center justify-center p-4 bg-gray-50">
-            <div className="w-full max-w-4xl">
-                {!hasConsented ? <ConsentModal onConsent={() => setHasConsented(true)} /> : <Questionnaire questions={questions}/>}
-            </div>
-        </main>
+        <QuestionnaireProvider>
+            <main className="min-h-screen flex items-center justify-center p-4 bg-gray-50">
+                <div className="w-full max-w-4xl">
+                    {!hasConsented ? <ConsentModal onConsent={() => setHasConsented(true)}/> :
+                        <Questionnaire questions={questions}/>}
+                </div>
+            </main>
+        </QuestionnaireProvider>
     )
 }
 
--- src/presentation/questionnaire/QuestionnairePageViewModel.tsx
+++ src/presentation/questionnaire/QuestionnairePageViewModel.tsx
@@ -1,33 +1,3 @@
-import {useCallback, useEffect, useMemo, useState} from "react";
-import {Question} from "@/domain/model/Question.ts";
-import QuestionSupabaseDataSource from "@/data/datasource/supabase/QuestionSupabaseDataSource.ts";
-import {QuestionRepositoryDataSource} from "@/data/repository/QuestionRepositoryDataSource.ts";
-import QuestionGetAll from "@/domain/usecase/QuestionGetAll.ts";
-
 export default function QuestionnairePageViewModel() {
-    const [loading, setLoading] = useState<boolean>(true)
-    const [questions, setQuestions] = useState<Question[]>([])
 
-    const questionDataSource = useMemo(() => new QuestionSupabaseDataSource(), []);
-    const questionRepository = useMemo(() => new QuestionRepositoryDataSource(questionDataSource), [questionDataSource]);
-
-    const questionGetAllUseCase = useMemo(() => new QuestionGetAll(questionRepository), [questionRepository]);
-
-    const getAllQuestions = useCallback(async () => {
-        return await questionGetAllUseCase.invoke();
-    }, [questionGetAllUseCase]);
-
-    useEffect(() => {
-        if(loading) {
-            getAllQuestions().then(res => {
-                setQuestions(res);
-                setLoading(false);
-            });
-        }
-    }, [loading]);
-
-    return {
-        loading,
-        questions
-    }
 }
