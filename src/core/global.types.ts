a7c0acfad9b62e4c1572ba6ffdb7f1f1e9ee5837
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-10 19:53:43 +0700

    feat: global types

---
+++ src/core/global.types.ts
@@ -0,0 +1,7 @@
+import {Tables} from "@/core/supabase.ts";
+
+export type UserConsentForm = Tables<'user_consent_form'>;
+
+export type UserFinishSurveys = Tables<'user_finish_surveys'>;
+
+export type UserResponses = Tables<'user_responses'>;
