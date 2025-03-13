87987417352dbe07183951c0b8a0e00cc94e3263
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-14 02:11:28 +0700

    feat: better product page

--- src/presentation/product/ProductPage.tsx
+++ src/presentation/product/ProductPage.tsx
@@ -9,7 +9,7 @@ interface Props {
     category: RecordCategory;
 }
 
-export const ProductPage = ({category} : Props) => {
+export const ProductPage = ({category}: Props) => {
     const {
         records,
         recordsError,
@@ -20,40 +20,49 @@ export const ProductPage = ({category} : Props) => {
 
     const length = 4;
 
+    if (recordsError) {
+        return (
+            <div>Error</div>
+        )
+    }
+
+    if (recordsLoading) {
+        Array.from({length: length}).map((_, i) => (
+            <SkeletonCard key={i}/>
+        ))
+    }
+
     return (
         <>
             {
-                recordsLoading ?
-                    Array.from({length: length}).map((_, i) => (
-                        <SkeletonCard key={i} />
-                    )) :
-                    records.map((o, index) => (
-                        <>
-                            <RecordCard
-                                title={o.record_title}
-                                description={o.record_description}
-                                profit={12.5}
-                                price={5000000}
-                                risk={30}
-                                onWin={onPurchaseWin}
-                                onLose={onPurchaseLose}
-                            />
-                            <ProductCard
-                                key={index}
-                                title={o.record_title}
-                                description={o.record_description}
-                                icon={<PiggyBank className="h-6 w-6"/>}
-                                isPopular={true}
-                                primaryInfo={{label: "Bunga", value: "2,5% p.a."}}
-                                secondaryInfo={{label: "Setoran Minimal", value: "Rp 50.000"}}
-                                features={["test1", "test2", "test3"]}
-                                benefits={["test1", "test2", "test3"]}
-                                risks={["test1", "test2", "test3"]}
-                                ctaText="Buka Sekarang"
-                                onCtaClick={() => console.log(`Buka ${o.record_title}`)}
-                            />
-                        </>
-                    ))}
+                records &&
+                records.map((o, index) => (
+                    <>
+                        <RecordCard
+                            title={o.record_title}
+                            description={o.record_description}
+                            profit={12.5}
+                            price={5000000}
+                            risk={30}
+                            onWin={onPurchaseWin}
+                            onLose={onPurchaseLose}
+                        />
+                        <ProductCard
+                            key={index}
+                            title={o.record_title}
+                            description={o.record_description}
+                            icon={<PiggyBank className="h-6 w-6"/>}
+                            isPopular={true}
+                            primaryInfo={{label: "Bunga", value: "2,5% p.a."}}
+                            secondaryInfo={{label: "Setoran Minimal", value: "Rp 50.000"}}
+                            features={["test1", "test2", "test3"]}
+                            benefits={["test1", "test2", "test3"]}
+                            risks={["test1", "test2", "test3"]}
+                            ctaText="Buka Sekarang"
+                            onCtaClick={() => console.log(`Buka ${o.record_title}`)}
+                        />
+                    </>
+                ))}
         </>
     )
 }
