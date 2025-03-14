2157226dc8d5e8efd08aca497f78f63fa1ac6503
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-14 22:34:57 +0700

    feat: implemented getTransactionHistory

--- src/presentation/product/ProductPageViewModel.ts
+++ src/presentation/product/ProductPageViewModel.ts
@@ -5,23 +5,52 @@ import {RecordGetByCategory} from "@/domain/usecase/RecordGetByCategory.ts";
 import {RecordCategory} from "@/domain/interface/RecordCategory.ts";
 import {useUser} from "@/presentation/context/UserContext.tsx";
 import {useSupabaseQuery} from "@/lib/hook/UseSupabaseQuery.ts";
+import {TransactionHistorySupabaseDataSource} from "@/data/datasource/supabase/TransactionHistorySupabaseDataSource.ts";
+import {TransactionHistoryRepositoryDatasource} from "@/data/repository/TransactionHistoryRepositoryDatasource.ts";
+import {TransactionHistoryGetById} from "@/domain/usecase/TransactionHistoryGetById.ts";
+import {PostgrestError} from "@supabase/supabase-js";
+import {BaseSupabaseResponse} from "@/domain/model/response/BaseSupabaseResponse.ts";
 
 export default function ProductPageViewModel(category: RecordCategory) {
+    const {
+        user,
+        incBalance
+    } = useUser();
+
     const recordDataSource = useMemo(() => new RecordSupabaseDataSource(), []);
     const recordRepository = useMemo(() => new RecordRepositoryDataSource(recordDataSource), [recordDataSource]);
+
+    const transactionHistoryDataSource = useMemo(() => new TransactionHistorySupabaseDataSource(), []);
+    const transactionHistoryRepository = useMemo(() => new TransactionHistoryRepositoryDatasource(transactionHistoryDataSource), [transactionHistoryDataSource]);
 
     const recordGetByCategoryUseCase = useMemo(() => new RecordGetByCategory(recordRepository), [recordRepository]);
     const recordGetByCategory = useCallback(async () => {
         return await recordGetByCategoryUseCase.invoke(category)
     }, [recordGetByCategoryUseCase, category])
-
     const {
         data: records,
         error: recordsError,
         loading: recordsLoading,
         refetch: recordsRefetch,
     } = useSupabaseQuery(recordGetByCategory)
-    const {incBalance} = useUser();
+
+    const transactionHistoryGetByIdUseCase = useMemo(() => new TransactionHistoryGetById(transactionHistoryRepository), [transactionHistoryRepository]);
+    const getTransactionHistory = useCallback(async () => {
+        if (user === null) {
+            return {
+                success: false,
+                data: null,
+                error: {message: "User is not logged in", details: "", hint: "", code: ""} as PostgrestError
+            } as BaseSupabaseResponse<TransactionHistory[]>;
+        }
+        return await transactionHistoryGetByIdUseCase.invoke(user.id);
+    }, [transactionHistoryRepository, user]);
+    const {
+        data: transactionHistory,
+        error: transactionHistoryError,
+        loading: transactionHistoryLoading,
+        refetch: transactionHistoryRefetch,
+    } = useSupabaseQuery(getTransactionHistory)
 
     function onPurchaseWin(amount: number, profit: number) {
         const res = amount * (profit / 100);
@@ -38,6 +67,10 @@ export default function ProductPageViewModel(category: RecordCategory) {
         recordsError,
         recordsLoading,
         recordsRefetch,
+        transactionHistory,
+        transactionHistoryError,
+        transactionHistoryLoading,
+        transactionHistoryRefetch,
         onPurchaseWin,
         onPurchaseLose
     }
