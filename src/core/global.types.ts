d0fe242e150d98c5f693f94e2521b4f7d9ff929a
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-23 11:05:36 +0700

    feat: new globaltype system

--- src/core/global.types.ts
+++ src/core/global.types.ts
@@ -1,27 +1,33 @@
 import {Database, Tables} from "@/core/supabase.ts";
 
 declare global {
+    type DB = Database["public"]["Tables"]
+
     type User = Tables<'nudge_user'>;
-    type InsertUser = Database["public"]["Tables"]["nudge_user"]["Insert"];
+    type InsertUser = DB["nudge_user"]["Insert"];
 
     type UserConsent = Tables<'nudge_user_consent'>;
-    type InsertUserConsent = Database["public"]["Tables"]["nudge_user_consent"]["Insert"];
-    type UpdateUserConsent = Database["public"]["Tables"]["nudge_user_consent"]["Update"];
+    type InsertUserConsent = DB["nudge_user_consent"]["Insert"];
+    type UpdateUserConsent = DB["nudge_user_consent"]["Update"];
+
+    type UserSurvey = Tables<'nudge_user_survey'>
+    type InsertUserSurvey = DB["nudge_user_survey"]["Insert"];
+    type UpdateUserSurvey = DB["nudge_user_survey"]["Update"];
 
     type UserConsentForm = Tables<'user_consent_form'>;
-    type InsertUserConsentForm = Database["public"]["Tables"]["user_consent_form"]["Insert"];
+    type InsertUserConsentForm = DB["user_consent_form"]["Insert"];
 
     type UserFinishSurveys = Tables<'user_finish_surveys'>;
-    type InsertUserFinishSurvey = Database["public"]["Tables"]["user_finish_surveys"]["Insert"];
+    type InsertUserFinishSurvey = DB["user_finish_surveys"]["Insert"];
 
     type UserResponses = Tables<'user_responses'>;
-    type InsertUserResponse = Database["public"]["Tables"]["user_responses"]["Insert"];
+    type InsertUserResponse = DB["user_responses"]["Insert"];
 
     type PublicUser = Tables<'users'>;
-    type InsertPublicUser = Database["public"]["Tables"]["users"]["Insert"];
+    type InsertPublicUser = DB["users"]["Insert"];
 
     type TransactionHistory = Tables<'transaction_history'>;
-    type InsertTransactionHistory = Database["public"]["Tables"]["transaction_history"]["Insert"];
+    type InsertTransactionHistory = DB["transaction_history"]["Insert"];
 
     type Records = Tables<'records'>
 
