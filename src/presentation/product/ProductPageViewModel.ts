e9b5624f04d7e2b1e7cc1228bf71aaf373b616b8
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-23 16:11:04 +0700

    feat: implement create transaction

--- src/presentation/product/ProductPageViewModel.ts
+++ src/presentation/product/ProductPageViewModel.ts
@@ -14,6 +14,9 @@ import {ProductRepositoryDataSource} from "@/data/repository/ProductRepositoryDa
 import {ProductRead} from "@/domain/usecase/product/ProductRead.ts";
 import {data} from "autoprefixer";
 import {ProductReadyByCategory} from "@/domain/usecase/product/ProductReadyByCategory.ts";
+import {UserTransactionSupabaseDataSource} from "@/data/datasource/supabase/UserTransactionSupabaseDataSource.ts";
+import {UserTransactionRepositoryDataSource} from "@/data/repository/UserTransactionRepositoryDataSource.ts";
+import {UserTransactionCreate} from "@/domain/usecase/user_transaction/UserTransactionCreate.ts";
 
 export default function ProductPageViewModel(categoryId: number) {
     const {
@@ -26,6 +29,9 @@ export default function ProductPageViewModel(categoryId: number) {
 
     const productDataSource = useMemo(() => new ProductSupabaseDataSource(), []);
     const productRepository = useMemo(() => new ProductRepositoryDataSource(productDataSource), [productDataSource]);
+
+    const userTransactionDataSource = useMemo(() => new UserTransactionSupabaseDataSource(), []);
+    const userTransactionRepository = useMemo(() => new UserTransactionRepositoryDataSource(userTransactionDataSource), [userTransactionDataSource]);
 
     const transactionHistoryInsertUseCase = useMemo(() => new TransactionHistoryInsert(transactionHistoryRepository), [transactionHistoryRepository]);
     const insertTransactionHistory = useCallback(async (data: InsertTransactionHistory[]) => {
@@ -43,13 +49,18 @@ export default function ProductPageViewModel(categoryId: number) {
         refetch: productsRefetch,
     } = useSupabaseQuery(productRead)
 
-    function onPurchase(record: Record, win: boolean) {
+    const userTransactionCreateUseCase = useMemo(() => new UserTransactionCreate(userTransactionRepository), [userTransactionRepository]);
+    const userTransactionCreate = useCallback(async (data: InsertUserTransaction) => {
+        return await userTransactionCreateUseCase.invoke(data);
+    }, [userTransactionCreateUseCase])
+
+    function onPurchase(product: Product, win: boolean) {
         if(user === null) return;
-        insertTransactionHistory([{
-            record_id: record.id,
-            user_id: user.id,
+        userTransactionCreate({
+            nudge_user_id: user.id,
+            nudge_product_id: product.id,
             win: win
-        }] as InsertTransactionHistory[])
+        } as InsertUserTransaction)
         const res = 5000000 * (12.5 / 100);
         incBalance(win ? res : -res);
     }
