1b2f249b145395ea1f510b201f7aec844ca0d89d
Arya <64455337+AryaJayadi@users.noreply.github.com>
2025-03-23 10:17:42 +0700

    feat: implemented new login and register

--- src/presentation/context/UserContext.tsx
+++ src/presentation/context/UserContext.tsx
@@ -1,5 +1,5 @@
 import {createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState} from "react";
-import {AuthResponse, PostgrestError, User} from "@supabase/supabase-js";
+import {AuthResponse, PostgrestError} from "@supabase/supabase-js";
 import UserSupabaseDataSource from "@/data/datasource/supabase/UserSupabaseDataSource.ts";
 import {UserRepositoryDataSource} from "@/data/repository/UserRepositoryDataSource.ts";
 import {UserSignIn} from "@/domain/usecase/user/UserSignIn.ts";
@@ -20,8 +20,8 @@ interface UserContextType {
     setUser: (value: (((prevState: (User | null)) => (User | null)) | User | null)) => void;
     balance: number;
     setBalance: (value: (((prevState: number) => number) | number)) => void;
-    login: (email: string, password: string) => Promise<AuthResponse>;
-    register: (email: string, password: string) => Promise<AuthResponse>;
+    login: (data: InsertUser) => Promise<BaseSupabaseResponse<User>>;
+    register: (data: InsertUser) => Promise<BaseSupabaseResponse<User>>;
     logout: () => void;
     incBalance: (amount: number) => number;
     onConsent: () => void;
@@ -47,7 +47,7 @@ const UserContext = createContext<UserContextType>({
 });
 
 export function UserProvider({children}: { children: ReactNode }) {
-    const [value, setValue,] = useLocalStorage<AuthResponse | null>('auth', null)
+    const [value, setValue,] = useLocalStorage<User | null>('auth', null)
     const [user, setUser] = useState<User | null>(null);
     const [balance, setBalance] = useState<number>(1000000000);
     const location = useLocation();
@@ -126,13 +126,26 @@ export function UserProvider({children}: { children: ReactNode }) {
     } = useSupabaseQuery(checkSurvey);
 
     useEffect(() => {
+        // if (user === null) {
+        //     if (isAuthenticated(value?.data.session)) {
+        //         if (value?.data.user) {
+        //             setUser(value.data.user);
+        //         } else if (!WHITELIST_PATH.includes(location.pathname)) {
+        //             navigate(LOGIN_PATH, {state: {from: location}});
+        //         }
+        //     } else if (!WHITELIST_PATH.includes(location.pathname)) {
+        //         navigate(LOGIN_PATH, {state: {from: location}});
+        //     }
+        // } else if (user) {
+        //     if (hasConsentData === false) {
+        //         navigate(CONSENT_PATH, {state: {from: location}});
+        //     } else if (hasSurveyData === false) {
+        //         navigate(QUESTIONNAIRE_PATH, {state: {from: location}});
+        //     }
+        // }
         if (user === null) {
-            if (isAuthenticated(value?.data.session)) {
-                if (value?.data.user) {
-                    setUser(value.data.user);
-                } else if (!WHITELIST_PATH.includes(location.pathname)) {
-                    navigate(LOGIN_PATH, {state: {from: location}});
-                }
+            if (value) {
+                setUser(value)
             } else if (!WHITELIST_PATH.includes(location.pathname)) {
                 navigate(LOGIN_PATH, {state: {from: location}});
             }
@@ -145,17 +158,17 @@ export function UserProvider({children}: { children: ReactNode }) {
         }
     }, [user, value, navigate, location]);
 
-    async function login(email: string, password: string) {
-        const res = await userSignIn(email, password);
+    async function login(data: InsertUser) {
+        const res = await userSignIn(data);
 
         if (res.error) {
             console.log(res.error);
-        } else if (res.data.user == null) {
+        } else if (res.data == null) {
             console.log(res);
         }
 
-        setValue(res);
-        setUser(res.data.user);
+        setValue(res.data);
+        setUser(res.data);
         hasConsentRefetch();
         hasSurveyRefetch();
 
@@ -164,34 +177,29 @@ export function UserProvider({children}: { children: ReactNode }) {
         return res;
     }
 
-    async function register(email: string, password: string) {
-        const res = await userSignUp(email, password);
+    async function register(data: InsertUser) {
+        const res = await userSignUp(data);
 
         if (res.error) {
             console.log(res.error);
             return res;
-        } else if (res.data.user == null) {
+        } else if (res.data == null) {
             console.log(res);
             return res;
         }
 
-        const userId = res.data.user.id;
+        const userId = res.data.id;
 
-        publicUserInsert({
-            id: userId,
-            email: res.data.user.email
-        }).then(async () => {
-            await Promise.allSettled([
-                userFinishConsent({
-                    user_id: userId,
-                    consent_agreement: false
-                }),
-                userFinishSurvey({
-                    user_id: userId,
-                    has_finished: false
-                })
-            ]);
-        })
+        await Promise.allSettled([
+            userFinishConsent({
+                user_id: userId,
+                consent_agreement: false
+            }),
+            userFinishSurvey({
+                user_id: userId,
+                has_finished: false
+            })
+        ]);
 
         navigate(LOGIN_PATH, {replace: true});
 
